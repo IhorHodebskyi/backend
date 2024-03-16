2bc84d74be41145b893b809a621a45db022d8b1b
ihor <i0962112822@gmail.com>
2024-03-16 20:33:16 +0200

    add

--- services/authServices.js
+++ services/authServices.js
@@ -10,16 +10,12 @@ const signup = async (body) => {
   const { name, email, password } = body;
   let sql =
     "SELECT `id`, `email`, `name` FROM `users` WHERE `email` = '" + email + "'";
-  //   let sql = `SELECT id, name, email FROM users WHERE email = '${email}'`;
   const conn = await mysql.createConnection(config);
   const rows = await conn.execute(sql);
-  console.log(rows.length > 0);
-  if (rows.length > 0) {
+  if (rows[0].length > 0) {
     conn.end();
     throw HttpError(409, "Email in use");
-    // res.status(409).json({ message: "Email in use" });
   }
-  console.log("object");
   const salt = bcrypt.genSaltSync(15);
   const hashPassword = bcrypt.hashSync(password, salt);
   const payload = {
@@ -39,7 +35,7 @@ const signup = async (body) => {
     token +
     "')";
   const results = await conn.execute(sql);
-  console.log(results[0].insertId);
+  conn.end();
   return {
     token,
     user: {
